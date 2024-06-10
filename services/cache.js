97390d3a06915d93b2ca33d391abba77a840c8d6
Powerwo1f <belous.aleksei18@gmail.com>
2024-06-10 15:33:11 +0200

    monkey patching mongoose exec and creating cache method

--- services/cache.js
+++ services/cache.js
@@ -1,8 +1,39 @@
 const mongoose = require('mongoose');
+const util = require('util');
 
+const redis = require('redis');
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-    console.log('running the query...');
-    return exec.apply(this, arguments);
+mongoose.Query.prototype.cache = async function () {
+    this.useCache = true;
+    return this;
+}
+
+mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
+    }
+
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    }));
+    console.log(key);
+
+    const cacheValue = await client.get(key);
+
+    if (cacheValue) {
+        const doc = JSON.parse(cacheValue);
+
+        return Array.isArray(doc)
+            ? doc.map(d => new this.model(d))
+            : new this.model(doc);
+    }
+
+    const result = await exec.apply(this, arguments);
+
+    client.set(key, JSON.stringify(result));
+    return result;
 }
