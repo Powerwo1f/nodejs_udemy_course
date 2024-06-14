b96745140318525769aef48ce812d81699e1a246
Powerwo1f <belous.aleksei18@gmail.com>
2024-06-14 03:23:38 +0200

    roundgoing of google oauth for tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,15 +1,48 @@
 const puppeteer = require('puppeteer');
 
-test('1 + 2', () => {
-    const sum = 1 + 2;
+let browser, page;
 
-    expect(sum).toEqual(3);
-})
-
-test('We can launch a browser', async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        // headless: false,
     });
-    const page = await browser.newPage();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+});
 
-})
+afterEach(async () => {
+    await browser.close();
+});
+
+test('Header has the correct text', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
+});
+
+test('Clicking on login starting OAuth flow', async () => {
+    await page.click('.right a');
+
+    const url = await page.url();
+
+    expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+    const id = '';
+
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    };
+    const sessionEncodedJSON = JSON.stringify((sessionObject)).toString('base64');
+    const sessionString = Buffer.from(sessionEncodedJSON);
+
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+
+});
