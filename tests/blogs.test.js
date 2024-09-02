87e3b38be5cb73b10eacaacd45bd70a36957dafc
Powerwo1f <belous.aleksei18@gmail.com>
2024-09-03 01:05:05 +0200

    added test for checking form validation

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,21 +1,38 @@
 const Page = require('./helpers/page');
 
+let page;
+
+beforeEach(async () => {
+    page = await Page.build();
+    await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+    await page.close();
+});
+
 describe('When logged in', async () => {
-    let page;
 
     beforeEach(async () => {
-        page = await Page.build();
-        await page.goto('http://localhost:3000');
-    });
-
-    afterEach(async () => {
-        await page.close();
+        await page.login();
+        await page.click('a.btn-floating');
     });
 
     test('can see blog create form', async () => {
-        await page.login();
-        await page.click('a.btn-floating');
         const label = await page.getContentsOf('form label');
         expect(label).toEqual('Blog Title');
+    });
+
+    describe('And using invalid inputs', async () => {
+        beforeEach(async () => {
+            await page.click('form button');
+        });
+
+        test('the form shows an error message', async () => {
+            const titleError = await page.getContentsOf('.title .red-text');
+            const ContentError = await page.getContentsOf('.content .red-text');
+            expect(titleError).toEqual('You must provide a value');
+            expect(ContentError).toEqual('You must provide a value');
+        });
     });
 });
