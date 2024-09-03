5938df523edbe14d3617744d0ccb4b28bc10981c
Powerwo1f <belous.aleksei18@gmail.com>
2024-09-03 02:18:48 +0200

    added two more tests for checking API restrictions

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -61,3 +61,34 @@ describe('When logged in', async () => {
         });
     });
 });
+
+describe('User is not logged in', async () => {
+    test('User cannot create blog posts', async () => {
+       const result = await page.evaluate(() => {
+           return fetch('/api/blogs', {
+               method: 'POST',
+               credentials: 'same-origin',
+               headers: {
+                   'Content-Type': 'application/json'
+               },
+               body: JSON.stringify({ title: 'My Title', content: 'My Content' })
+           }).then(res => res.json());
+       });
+
+       expect(result).toEqual({ error: 'You must log in!' });
+    });
+
+    test('User cannot get a list of posts', async () => {
+        const result = await page.evaluate(() => {
+            return fetch('/api/blogs', {
+                method: 'GET',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json());
+        });
+
+        expect(result).toEqual({ error: 'You must log in!' });
+    });
+});
