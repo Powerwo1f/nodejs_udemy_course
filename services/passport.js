f68111f21b4294db2bcad0c418c4226445c9fd88
Powerwo1f <belous.aleksei18@gmail.com>
2024-09-03 23:04:40 +0200

    requiring User model in passport.js

--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
+require('../models/User');
 
 const User = mongoose.model('User');
 
