8d123bc5624aa812d2d841d56a213dae73e23076
Powerwo1f <belous.aleksei18@gmail.com>
2024-09-03 23:57:30 +0200

    4. consoling env values in CI

--- tests/setup.js
+++ tests/setup.js
@@ -4,6 +4,7 @@ require('../models/User');
 
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
+console.log(keys);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, {
